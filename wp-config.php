ae9ce99fb2a879cf148baee2fdf3f0872215aa52
0xAckerMan <joelkores1@gmail.com>
2023-05-27 13:16:15 +0300

    chore: Creates tables

---
+++ wp-config.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the installation.
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
+ * and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * Database settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://wordpress.org/documentation/article/editing-wp-config-php/
+ *
+ * @package WordPress
+ */
+
+// ** Database settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'schedulr' );
+
+/** Database username */
+define( 'DB_USER', 'admin' );
+
+/** Database password */
+define( 'DB_PASSWORD', 'admin' );
+
+/** Database hostname */
+define( 'DB_HOST', 'localhost' );
+
+/** Database charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8mb4' );
+
+/** The database collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '?d9Zhy=brEY6|9`xaAH8<=3b<*sl/5H|h%>*1TvSTq{c#[A.b$LB zgO9jy~05]F' );
+define( 'SECURE_AUTH_KEY',  'Bwk]S4DQ17R=w=GMTkF/]#{}M?{np)%DoEcgb])i&S6+,t4lhQi7K7F;E(-RoKa8' );
+define( 'LOGGED_IN_KEY',    '9c&uYZb>pHm.f>R#q5;kN Y;vJL@L4p-xu5jP-$B!7!37QH9Da~uukE$pl;o,T8T' );
+define( 'NONCE_KEY',        'sC~0dOcFsui-.v,I;*`7PWz*w=~zj:[2[<@[UQz1Y~q60j{J+}.A(A[<@L+y!ZH`' );
+define( 'AUTH_SALT',        'JCR^V[c$g~j .[)BID!7tAazx8A2r2pj5F;H:X1,-7C+0?H|PSG3Wm69n>&=o4*Y' );
+define( 'SECURE_AUTH_SALT', 'F,i0wp4g>m85qvvoxVZ/hzr[O{#.EoRc__2>&.57+LHLY%KIAWE<c^USV(nd*dCt' );
+define( 'LOGGED_IN_SALT',   'a}~DWKuHNG+D[-&Ad!*U9V!906%8U9(m1^9!F51rbob(5HIb#&1Va>%gc<@![5XW' );
+define( 'NONCE_SALT',       't:3VEtvmvrePSwfj9F8Q|Hd2[Z4> o{Wks>poVDo;=HZoOXTjZTBY_Gmp00:OvDE' );
+
+/**#@-*/
+
+/**
+ * WordPress database table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the documentation.
+ *
+ * @link https://wordpress.org/documentation/article/debugging-in-wordpress/
+ */
+define( 'WP_DEBUG', false );
+
+/* Add any custom values between this line and the "stop editing" line. */
+
+
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', __DIR__ . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once ABSPATH . 'wp-settings.php';
---
+++ wp-content/plugins/project-manager/project-manager.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @package project-manager
+ */
+
+/*
+    Plugin Name: Project Manager
+    Plugin URI: http://www.project-manager.com 
+    Author: John Doe
+    Description: This is a plugin for managing projects
+    Version: 1.0.0
+    License: GPLv2 or later
+    Text Domain: project-manager
+*/
+
+defined('ABSPATH') or die('Hey, hacker! you are the one pwned');
+
+
+
+
+
+class ProjectManager{
+
+    public function activate(){
+        $this->create_projects_table();
+    }
+
+    public function create_projects_table(){
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'projects';
+        $sql = "CREATE TABLE $table_name (
+            p_id mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            p_name varchar(100) NOT NULL,
+            P_description text NOT NULL,
+            p_assigned_to mediumint(9) NOT NULL,
+            p_due_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+            p_done integer default 0
+        )";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+    //tasks
+    //t_project_id, t_id, t_name, t_done,
+    public function create_tasks_table(){
+        global $wpdb;
+        $table_name = $wpdb->prefix . 'projects';
+        $sql = "CREATE TABLE $table_name (
+            t_id mediumint(9) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            t_project_id INTEGER NOT NULL,
+            t_name text NOT NULL,
+            t_done integer default 0
+        )";
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+    }
+}
+$project_manager = new ProjectManager();
+
+register_activation_hook(__FILE__, [$project_manager, 'activate']);

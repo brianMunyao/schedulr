6557a4d68f7eaf2a0f9405757d8fd00884a7e650
0xAckerMan <joelkores1@gmail.com>
2023-05-29 09:00:58 +0300

    CHORE: User Managemet endpoit

--- wp-content/plugins/project-manager/user_roles.php
+++ wp-content/plugins/project-manager/user_roles.php
@@ -3,7 +3,13 @@ class UserRoles{
     public function add_role(){
         add_role('ProjectManager', 'Project Manager', array(
             'read' => true,
-            'edit_posts' => false,
+            'edit_posts' => true,
+            'delete_posts' => true,
+        ));
+
+        add_role('Employee', 'Employee', array(
+            'read' => true,
+            'edit_posts' => true,
             'delete_posts' => false,
         ));
     }

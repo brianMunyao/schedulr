6e590c7c85970a6973c0114c62fdb0edfbc49cdc
brianMunyao <brianmunyao6@gmail.com>
2023-06-04 10:44:17 +0300

    updated db to capture shortcode

---
+++ wp-content/themes/schedulr-theme/page-chat.php
@@ -0,0 +1,17 @@
+<?php if (!is_user_logged_in()) wp_redirect(site_url('/login')) ?>
+
+
+
+<?php
+/**
+ * 
+ * Template Name: Chat Page Template
+ */
+get_header();
+?>
+<div class="page-chat">
+
+    chat here
+</div>
+
+<?php get_footer() ?>
